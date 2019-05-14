d4c145a1257e17e56055a2d5eb92cb8084694107
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-13 21:58:07 -0700

    Routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,30 @@ module.exports = app => {
 
     app.post('/api/friends', (req, res) => {
         let newFriend = req.body
+        let newScore = newFriend.scores
+
+        let name = ''
+        let img = ''
+
+        let totalDif = 1000
+
+        for (let i = 0; match.length; i++) {
+            let dif = 0
+
+            for (let j = 0; newScore.length; j++) {
+                dif += Math.abs(match[i].scores[j] - newScore.scores[j])
+            }
+
+            if (dif < totalDif) {
+                let name = match[i].name
+                let img = match[i].photo
+
+                let totalDif = dif
+            }
+        }
+        match.push(newFriend)
+
+        res.json({ status: 'OK', name: name, img: img })
     })
 
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,11 @@
-const match = require('../data/friends')
-
 module.exports = app => {
 
     app.get('/survey', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'))
+    })
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'))
     })
 
 }
